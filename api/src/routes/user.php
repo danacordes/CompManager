2d26950f9f26513c13d1fe890186f829edcfa720
Dana Cordes <dcordes@pudding.org>
2017-04-08 06:18:28 +0000

    Updatings registration validation rules to include all possible inputs to register endpoint.  Mapping those new fields in, and updating db, completeing basic registion logic

--- api/src/routes/user.php
+++ api/src/routes/user.php
@@ -21,13 +21,26 @@ $app->group('/user', function() use ($app) {
 
   });
 
-  $passwordValidator  = v::length(6 ,20);
+  $passwordValidator  = v::length(6, 90);
   $emailValidator     = v::email();
-  $nameValidator      = v::length(6 ,50);
-  $validator = array(
-    'password'  => $passwordValidator,
-    'email'     => $emailValidator,
-    'name'      => $nameValidator
+  $nameValidator      = v::length(6, 255);
+  $clubValidator      = v::optional(v::length(1, 255));
+  $addressValidator   = v::optional(v::length(1, 255));
+  $stateValidator     = v::optional(v::length(1, 5));
+  $ids                = v::optional(v::intVal());
+
+  $registerValidator = array(
+    'password'        => $passwordValidator,
+    'email'           => $emailValidator,
+    'name'            => $nameValidator,
+    'club'            => $clubValidator,
+    'address_1'       => $addressValidator,
+    'address_2'       => $addressValidator,
+    'city'            => $addressValidator,
+    'state'           => $stateValidator,
+    'zip'             => $stateValidator,
+    'competition_id'  => $ids,
+    'organization_id' => $ids,
   );
 
   $app->post('/register', function ($request, $response, $args )use ($app){
@@ -40,19 +53,26 @@ $app->group('/user', function() use ($app) {
 
     $params = $request->getQueryParams();
     //TODO: throw validation exceptions
-    //TODO: implement rest of user field
     //TODO: implement org, comp, etc, associations
     $userData = [];
 
-    $userData['name'] = $params['name']; 
-    $userData['email'] = $params['email']; 
-    $userData['password'] = password_hash($params['password'], PASSWORD_DEFAULT); 
+    //required fields
+    $userData['name']       = $params['name']; 
+    $userData['email']      = $params['email']; 
+    $userData['password']   = password_hash($params['password'], PASSWORD_DEFAULT); 
+    //optional fields
+    if(!empty($params['club']))       $userData['club']       = $params['club']; 
+    if(!empty($params['address_1']))  $userData['address_1']  = $params['address_1']; 
+    if(!empty($params['address_2']))  $userData['address_2']  = $params['address_2']; 
+    if(!empty($params['city']))       $userData['city']       = $params['city']; 
+    if(!empty($params['state']))      $userData['state']      = $params['state']; 
+    if(!empty($params['zip']))        $userData['zip']        = $params['zip']; 
 
     //confirm uniqueness of email address
     if(!User::isEmailUnused($userData['email'])){
       return $response->withJSON(['error' => "The email address '{$userData['email']}' has already been registered.  If you've had one to many and can't remember you password, please hit the 'Reset Password' button."]);
     }
-die(print_r($userData, true));
+//die(print_r($userData, true));
     
     $user = User::create($userData);
 
@@ -62,7 +82,7 @@ die(print_r($userData, true));
 
     return $response->withJSON($result);
     
-  })->add(new \DavidePastore\Slim\Validation\Validation($validator));
+  })->add(new \DavidePastore\Slim\Validation\Validation($registerValidator));
 
 });
 ?>
